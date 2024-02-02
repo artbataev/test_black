c6a19cb08d3194fd473c2ba725c1a0c9952d5c4c
Vladimir Bataev <vbataev@nvidia.com>
2024-02-03 00:25:49 +0400

    Change with incorrect formatting

--- useless_package/useless_cls.py
+++ useless_package/useless_cls.py
@@ -1,7 +1,10 @@
 
 class StrangeFormattedClass:
 
-    def __init__(self, x):
+    def __init__(self,
+
+
+                 x):
         """ Some docstring very very very long long long long long long long long long long long long long long long long long long long long long
             """
         self.x = x
